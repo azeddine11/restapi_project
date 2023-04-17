ae7ed5ecd94180301319013a623ed0180b9892df
Azeddine <115009714+azeddine11@users.noreply.github.com>
2023-04-17 21:22:59 +0000

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 
-
+// Importing Mongoose library
 const mongoose = require('mongoose');
 
+// Defining a new Mongoose schema for 'User'
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -12,7 +13,7 @@ const userSchema = new mongoose.Schema({
     required: true
   }
 });
-
+// Creating a Mongoose model for 'User' using the defined schema
 const User = mongoose.model('User', userSchema);
-
+// Exporting the 'User' model to be used in other files
 module.exports = User;
